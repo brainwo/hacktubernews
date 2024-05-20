d77660759e546d558c836a431c56200815a0f010
Brian Wo <45139213+brainwo@users.noreply.github.com>
2024-05-20 10:38:37 +0800

    fix: linter languageOptions

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
+
